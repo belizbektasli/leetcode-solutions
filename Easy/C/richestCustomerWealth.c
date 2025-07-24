0b6f01c3fd207ff3ebe5ec52012c7fa193d572da
Beliz <beliz20045@gmail.com>
2025-07-24 17:20:27 +0300

    Update richestCustomerWealth.c

--- Easy/C/richestCustomerWealth.c
+++ Easy/C/richestCustomerWealth.c
@@ -1,8 +1,9 @@
+//This program finds the richest customer with the most wealthy bank account.
 int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
     int maxWealth = 0;
     for (int i = 0;i<accountsSize;i++){
-        int wealth = 0;
-        for(int j = 0;j<accountsColSize[i];j++){
+        int wealth = 0; //Updating wealth to  zero after each loop so we get the wealth for each customer not the sum of all accounts.
+        for(int j = 0;j<accountsColSize[i];j++){ //Using accountsColSize[i] because ColSize is a pointer not a direct integer.
             wealth += accounts[i][j];
             if (wealth > maxWealth){
                 maxWealth = wealth;
